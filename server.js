d5006230ed72a0036ffd1261c872c4f67c1a733b
manutd2008thebest <manutd2008thebest@gmail.com>
2025-03-31 14:12:27 +0500

    fix: fix cors

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
